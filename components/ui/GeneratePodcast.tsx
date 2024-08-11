a2e478f39c2905e873b1944a9e17f4b8ac51a9eb
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-08-11 21:39:53 +0530

    create podcast page

---
+++ components/ui/GeneratePodcast.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  setAudio,
+  voiceType,
+  audio,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration,
+}: GeneratePodcastProps) => {
+  return (
+    <div>GeneratePodcast</div>
+  )
+}
+
+export default GeneratePodcast
