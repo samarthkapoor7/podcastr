ba913f32c7c49e41dfa9ccea124a63abd5f6f0b6
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-08-30 17:46:10 +0530

    changes

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, value = 0, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
